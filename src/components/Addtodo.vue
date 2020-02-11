6acd3ef9706515d0f8cf1c79e26811443c54cd2c
unknown <bonificial@yahoo.com>
2020-02-11 14:01:39 +0300

    Updated

---
+++ src/components/Addtodo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Item" />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name:"AddTodo",
+    data(){
+        return{
+title:''
+            }
+            },
+            methods:{
+addTodo(e){
+    e.preventDefault();
+    
+    const newToDo = {
+ 
+title:this.title,
+completed:false
+        }
+        this.$emit('add-todo',newToDo);
+        this.title = "";
+        }
+                }
+})
+</script>
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the todo app version 1.0.0 which is part of the vue crash course</p>
+  </div>
+</template>
+/** 200-12905 **/
